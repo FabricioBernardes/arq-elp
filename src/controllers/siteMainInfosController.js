58acef77b12c4fe3856c51d84468581efd4aba1d
Fabricio Bernardes <fbernardes.dev@gmail.com>
2022-09-02 21:35:54 -0300

    feat: passa a utilizar os dados cadastrados no banco de dados e exclui os cadastros estáticos

--- src/controllers/siteMainInfosController.js
+++ src/controllers/siteMainInfosController.js
@@ -3,6 +3,9 @@ const Site = require('../models/SiteModel');
 exports.index = async(req, res) => {
   const sites = await Site.buscaSites();
   const urlParams =  req.params.siteId;
-  res.render('site-main-page', { urlParams });
+  const siteSelected = sites.filter( (item)=>{
+    return item._id == urlParams
+  })
+  res.render('site-main-page', { urlParams, siteSelected });
 };
 
